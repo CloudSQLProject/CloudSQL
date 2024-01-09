256201c40431743b01f8fc5c081a7b0766415f81
Anorochc <99007899+Anorochc@users.noreply.github.com>
2024-01-09 16:31:06 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,113 @@
+import json
+import os
+
+loginFlag = False
+from create_table import create_table,add_record
+
+def create_user_database():
+    if not os.path.exists('./users.json'):
+        with open('./users.json', 'w') as file:
+            initial_data = {'root': {'password': 'root', 'type': 1}}
+            json.dump(initial_data, file)
+
+def login(username, password):
+    with open('./users.json', 'r') as file:
+        users = json.load(file)
+        if username in users :
+            if users[username]['password'] == password:
+                print('Login successful. User type:', users[username]['type'])
+                user_type = users[username]['type']
+                return True, user_type
+            else:
+                print(f'User {username} Wrong Password')
+                return False, 0
+        else:
+            print(f'User {username} Not Found')
+            return False, 0
+
+def add_user(username, password):
+    global loginFlag, user_type
+    if loginFlag:
+        if user_type == 1:
+            with open('./users.json', 'r') as file:
+                users = json.load(file)
+                if username in users:
+                    print('User already exists')
+                    return
+                users[username] = {'password': password, 'type': 0}
+                with open('./users.json', 'w') as file:
+                    json.dump(users, file, indent=4)
+                print('User added successfully')
+        else:
+            print('Low Authorization')
+    else:
+        print('Please login first')
+
+
+def grant_authorization(username):
+    global loginFlag, user_type
+    if loginFlag:
+        if user_type == 1:
+            with open('./users.json', 'r') as file:
+                users = json.load(file)
+                if username in users:
+                    users[username]['type'] = 1
+                    with open('./users.json', 'w') as file:
+                        json.dump(users, file, indent=4)
+                    print(f'Authorization granted to {username}')
+                else:
+                    print(f'User {username} not found')
+        else:
+            print('Low Authorization')
+    else:
+        print('Please login first')
+
+
+def drop(username):
+    global loginFlag, user_type
+    if loginFlag:
+        if user_type == 1:
+            with open('./users.json', 'r') as file:
+                users = json.load(file)
+                if username in users:
+                    del users[username]
+                    with open('./users.json', 'w') as file:
+                        json.dump(users, file, indent=4)
+                    print(f'{username} Deleted')
+                else:
+                    print(f'User {username} not found')
+        else:
+            print('Low Authorization')
+    else:
+        print('Please login first')
+
+
+
+if __name__ == "__main__":
+    create_user_database()
+    while True:
+        command = input()
+        if command.startswith("login"):
+            _, username, password = command.split()
+            loginFlag, user_type = login(username, password)
+            if loginFlag:
+                print('Welcome to the database')
+
+        elif command.startswith("add_user"):
+            _, username, password = command.split()
+            add_user(username, password)
+
+        elif command.startswith("grant"):
+            _, username = command.split()
+            grant_authorization(username)
+
+        elif command.startswith("drop"):
+            _, username = command.split()
+            drop(username)
+
+
+
+
+        elif command.startswith("exit"):
+            loginFlag = False
+            break
