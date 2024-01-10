5ae736a228fe547b8c654bea2e7b74299b9afde9
Anorochc <99007899+Anorochc@users.noreply.github.com>
2024-01-10 11:19:09 +0800

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -1,6 +1,9 @@
 import json
 import os
 
+# 全局变量用来存放每个表格中存在的列名
+table_columns = {}
+
 def create_table(table_name, *columns):
     table_file = f'./{table_name}.json'
     if not os.path.exists(table_file):
@@ -8,6 +11,10 @@ def create_table(table_name, *columns):
             initial_data = []
             json.dump(initial_data, file)
         print(f'Table {table_name} created successfully with columns: {columns}')
+        global table_columns
+        table_columns[table_name] = list(columns)
+        print(table_columns[table_name])
+        print(table_columns)
     else:
         print(f'Table {table_name} already exists')
 
