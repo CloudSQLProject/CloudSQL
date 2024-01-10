0dcfe550adf4b3467fbec9b172efe374dc6a0e86
23211 <2321136573@qq.com>
2024-01-10 10:51:10 +0800

    select方法第一版

--- select_table.py
+++ select_table.py
@@ -13,12 +13,12 @@ def select(table_name, static_string):
     else:
         with open(table_file) as f:
             response = json.load(f)
-    table = PrettyTable()
-    fieldName = ['id', 'name', 'age']
-    table.field_names = (fieldName)
+    table = PrettyTable()  # 制表
+    keys = [row.keys() for row in response]
+    table.field_names = keys[0]  # 将第一个key值作为表头
     for row in response:
         values = list(row.values())
-        table.add_row(values)
+        table.add_row(values)  # 添加value
     print(table)
 
 
