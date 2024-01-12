173c1ed066d2619ad42a4deba8d6abceee58bc0b
Anorochc <1041173672@qq.com>
2024-01-12 17:57:30 +0800

    update select except WHERE

--- create_table.py
+++ create_table.py
@@ -9,7 +9,6 @@ def create_table(table_name, *columns):
         with open(table_file, 'w') as file:
             initial_data = []
             json.dump(initial_data, file)
-
         columns = [column.strip(',') for col_group in columns for column in col_group.split(',')]
         print(f'Table {table_name} created successfully with columns: {columns}')
         global table_columns
@@ -24,19 +23,14 @@ def add_record(table_name, values):
     if os.path.exists(table_file):
         with open(table_file, 'r') as file:
             data = json.load(file)
-
         columns = table_columns.get(table_name, [])
-
         if not columns:
             print(f'Columns for table {table_name} not found')
             return
-
         if len(values) != len(columns):
             print(f'Error: Number of values does not match the number of columns for table {table_name}')
             return
-
         record = {column: value for column, value in zip(columns, values)}
-
         if any(all(record.get(key) == value for key, value in row.items()) for row in data):
             print('Record already exists')
         else:
@@ -51,24 +45,16 @@ def create_table_main():
     while True:
         user_input = input("Enter command: ")
         parts = user_input.split()
-
         if len(parts) < 4:
             print('Invalid command')
             continue
-
         command = parts[0]
         table_name = parts[2]
-
         if command == 'create' and len(parts) > 3 and parts[3] == 'columns':
             columns = [column.strip(',') for column in parts[4:]]
             create_table(table_name, *columns)
-
         elif command == 'insert' and len(parts) > 3 and parts[3] == 'values':
             values = [value.strip('(),') for value in parts[4:]]
             add_record(table_name, values)
-
         else:
             print('Invalid command')
-
-if __name__ == '__main__':
-    main()
--- select_table.py
+++ select_table.py
@@ -1,40 +1,97 @@
 import json
 import os
-#from prettytable import PrettyTable
+import static_string
 
-def select(table_name, static_string):
-    table_file = f'./{table_name}.json'
-    if not os.path.exists(table_file):
-        print(static_string.TABLE_NOT_EXIST)
+class StaticString:
+    TABLE_NOT_EXIST = "Table does not exist"
+staticString = StaticString()
+
+
+def get_user_input(user_input):
+    parts = user_input.split(' ')
+    if len(parts) < 4 or parts[0].lower() != 'select' or parts[2].lower() != 'from':
+        print("Invalid command")
+        return None, None
     else:
-        with open(table_file) as f:
-            response = json.load(f)
+        aim = parts[1].split(',')
+        table = parts[3]
+        return aim,table
 
-    if len(response) == 0:
-        print("Table is empty")
-        return
-
-    keys = response[0].keys()
-    max_lengths = [len(str(key)) for key in keys]
-    rows = []
-
-    for row in response:
-        values = list(row.values())
-        rows.append(values)
-        max_lengths = [max(max_lengths[i], len(str(values[i]))) for i in range(len(values))]
-
+def draw_table(data, keys, max_lengths):
     header = "|".join([f"{key:<{max_lengths[i]}}" for i, key in enumerate(keys)])
     separator = "+".join(["-" * (max_lengths[i] + 2) for i in range(len(keys))])
     #print(separator)
     print(header)
     #print(separator)
-
-    for row in rows:
+    for row in data:
         formatted_row = "|".join([f"{value:<{max_lengths[i]}}" for i, value in enumerate(row)])
         print(formatted_row)
         #print(separator)
-class StaticString:
-    TABLE_NOT_EXIST = "Table does not exist"
 
-staticString = StaticString()
-select('student', staticString)
+
+def select_all(table_name,static_string):
+    table_file = f'./{table_name}.json'
+    if not os.path.exists(table_file):
+        print(static_string.TABLE_NOT_EXIST)
+        return []
+    else:
+        with open(table_file) as f:
+            response = json.load(f)
+    if len(response) == 0:
+        print("Table is empty")
+        return
+    keys = response[0].keys()
+    max_lengths = [len(str(key)) for key in keys]
+    rows = []
+    for row in response:
+        values = list(row.values())
+        rows.append(values)
+        max_lengths = [max(max_lengths[i], len(str(values[i]))) for i in range(len(values))]
+    draw_table(rows, keys, max_lengths)
+
+
+def select_column(table_name,aim):
+    if 'id' not in aim:
+        aim.insert(0, 'id') #这里的id就是主键，以后改
+
+    table_file = f'./{table_name}.json'
+    if not os.path.exists(table_file):
+        print("Table does not exist")
+        return []
+    else:
+        with open(table_file) as f:
+            response = json.load(f)
+            if len(response) == 0:
+                print("Table is empty")
+                return []
+            result = []
+            for row in response:
+                entry = {}
+                for key in aim:
+                    if key in row:
+                        entry[key] = row[key]
+                result.append(entry)
+            keys = result[0].keys()
+            max_lengths = [len(str(key)) for key in keys]
+            rows = []
+            for row in result:
+                values = list(row.values())
+                rows.append(values)
+                max_lengths = [max(max_lengths[i], len(str(values[i]))) for i in range(len(values))]
+            draw_table(rows, keys, max_lengths)
+
+
+
+def main():
+    while True:
+        user_input = input("Enter Command: ")
+        aim,table = get_user_input(user_input)
+        #print(aim,table)
+        if aim == ['*']:
+            select_all(table,static_string)
+        else:
+            (select_column(table,aim))
+
+
+if __name__ == "__main__":
+    main()
