16fa7121aa44c684a2f380ec8bb134a4e4601f7c
chd <1039421948@qq.com>
2024-01-15 15:03:27 +0800

    文件存储格式需要使用jason,目前实现create语句规范化
    详见代码注释和变动

--- field.py
+++ field.py
@@ -8,9 +8,9 @@ class Field:
            - primary_key: 是否为主键，布尔值
            - not_null: 是否允许为空，布尔值
            """
-    def __init__(self, name, data_type, primary_key, not_null):#
+    def __init__(self, name, data_type, primary_key, null_flag):#
         self.name = name
         self.data_type = data_type
         self.primary_key = primary_key
-        self.not_null = not_null
+        self.null_flag = null_flag
 
--- table_format.py
+++ table_format.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 
@@ -25,28 +26,33 @@ class Table:
         if not os.path.exists(table_directory):
             os.makedirs(table_directory)  # 如果目录不存在，就创建目录
         # 如果没有,创建文件,已有文件则追加写入数据
-        file_path = os.path.join(directory, self.table_name + ".txt")
+        file_path = os.path.join(table_directory, self.table_name + ".txt")
         with open(file_path, 'a' if os.path.exists(self.table_name + ".txt") else 'w') as f:# 追加写入数据
             if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
-                f.write(','.join(self.columns.keys())+'\n')
+                for column in self.columns:
+                    print("写入表名")
+                    f.write(column.name+' ')
+                f.write('\n')
             for row in self.data:
                 print(row)
                 print("-----------")
                 value_str=','.join(map(str, row.values()))
                 f.write(value_str+'\n')
             print("表数据写入成功!")
-        dict_path = os.path.join(directory, self.table_name + ".dict")
+        dict_path = os.path.join(table_directory, self.table_name + ".dict")
         if not os.path.exists(dict_path):
             with open(dict_path, 'w') as f:
-                f.write(str(self.columns))
+                for column in self.columns:
+                    f.write(f"name: {column.name},data_type: {column.data_type},primary_key: {column.primary_key},null_flag: {column.null_flag}\n")
+
 
 # 创建一个名为"employee"的表格，包含id, name, age三个字段
-employee_table = Table("employee", {"id": "int", "name": "str", "age": "int"})
-
-# 插入数据
-employee_table.insert({"id": 1, "name": "Alice", "age": 25})
-employee_table.insert({"id": 2, "name": "Bob", "age": 30})
-employee_table.insert({"id": 3, "name": "Charlie", "age": 28})
-print(employee_table.data)
-employee_table.save()
+# employee_table = Table("employee", {"id": "int", "name": "str", "age": "int"})
+#
+# # 插入数据
+# employee_table.insert({"id": 1, "name": "Alice", "age": 25})
+# employee_table.insert({"id": 2, "name": "Bob", "age": 30})
+# employee_table.insert({"id": 3, "name": "Charlie", "age": 28})
+# print(employee_table.data)
+# employee_table.save()
 
